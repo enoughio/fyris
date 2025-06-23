3cbc9aea294669d1f9aa545349842384030c7542
Ani <turboji007@gmail.com>
2025-06-23 12:51:51 +0530

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import Navbar from "@/components/navbar"
 import Footer from "@/components/footer"
+import { Analytics } from '@vercel/analytics/next';
+
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -39,7 +41,9 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           <div className="min-h-screen bg-gradient-to-br from-gray-950 via-gray-900 to-gray-950">
             <Navbar />
+
             <main>{children}</main>
+             <Analytics />
             <Footer />
           </div>
         </ThemeProvider>
